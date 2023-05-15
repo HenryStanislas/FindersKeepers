e3ec5e6adb45c6b4acf86c71fa824a9c813d2da8
HenryStanislas <113907325+HenryStanislas@users.noreply.github.com>
2023-05-15 15:02:46 +0200

    Delete platforms directory

--- platforms/browser/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "cordova-plugin-geolocation": "4.1.0"
-}
-// BOTTOM OF METADATA
-});
